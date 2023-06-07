967b1b274d8a78f341d8ae79cf73489325c31fea
zenzen <aaaaru66@naver.com>
2023-06-07 16:31:45 +0900

    [아연] 일반회원-신체정보 달력, 상세보기 기능 추가 06-07 16:31

--- FitQuest/src/main/java/physique/model/PhysiqueBean.java
+++ FitQuest/src/main/java/physique/model/PhysiqueBean.java
@@ -2,4 +2,86 @@ package physique.model;
 
 public class PhysiqueBean {
 
+	private int phnum;
+	private String id;
+	private double height;
+	private double weight;
+	private double bmi;
+	private String phdate; // 측정일
+	private double skeletalmuscle; // 골격근량
+	private double bodyfatper; // 체지방률
+	private int bmr; // 기초대사량
+
+	public int getPhnum() {
+		return phnum;
+	}
+
+	public void setPhnum(int phnum) {
+		this.phnum = phnum;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getBmi() {
+		return bmi;
+	}
+
+	public void setBmi(double bmi) {
+		this.bmi = bmi;
+	}
+
+	public String getPhdate() {
+		return phdate;
+	}
+
+	public void setPhdate(String phdate) {
+		this.phdate = phdate;
+	}
+
+	public double getSkeletalmuscle() {
+		return skeletalmuscle;
+	}
+
+	public void setSkeletalmuscle(double skeletalmuscle) {
+		this.skeletalmuscle = skeletalmuscle;
+	}
+
+	public double getBodyfatper() {
+		return bodyfatper;
+	}
+
+	public void setBodyfatper(double bodyfatper) {
+		this.bodyfatper = bodyfatper;
+	}
+
+	public int getBmr() {
+		return bmr;
+	}
+
+	public void setBmr(int bmr) {
+		this.bmr = bmr;
+	}
+
 }
--- FitQuest/src/main/java/physique/model/PhysiqueDao.java
+++ FitQuest/src/main/java/physique/model/PhysiqueDao.java
@@ -1,8 +1,27 @@
 package physique.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PhysiqueDao {
-
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	private String namespace = "physique.model.Physique";
+	
+	public PhysiqueBean getOnePhysique(Map<String, Object> map) {
+		PhysiqueBean physiqueBean = sqlSessionTemplate.selectOne(namespace + ".GetOnePhysique", map);
+		return physiqueBean;
+	}
+	public List<String> getPhysiqueByDate(Map<String, String> map) {
+		List<String> dateList = new ArrayList<String>();
+		dateList = sqlSessionTemplate.selectList(namespace + ".GetPhysiqueByDate", map);
+		return dateList;
+	}
 }
