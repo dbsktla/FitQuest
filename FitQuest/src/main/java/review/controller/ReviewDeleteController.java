0db57bbac93c27c06eb9259ba88bb2653ae80249
jwm21542 <jwm21542@gmail.com>
2023-06-13 14:32:17 +0900

    [종원] 일반 회원 리뷰 관리 06-13 02:32pm

--- FitQuest/src/main/java/review/controller/ReviewDeleteController.java
+++ FitQuest/src/main/java/review/controller/ReviewDeleteController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import review.model.ReviewBean;
 import review.model.ReviewDao;
 
 @Controller
@@ -14,13 +16,16 @@ public class ReviewDeleteController {
 	ReviewDao reviewDao;
 	@RequestMapping(value = command)
 	public String doAction(@RequestParam("renum") int renum) {
+		ReviewBean reviewBean = reviewDao.getReviewByRenum(renum);
+		int cnt = reviewDao.deleteReview(renum); //리뷰 삭제 시킴
 		
-		int cnt = reviewDao.deleteReview(renum);
-		
-		if(cnt > 0) {
+		if(cnt > 0) { 
 			System.out.println("리뷰 삭제 성공");
 		}
-		
+		int reviewCount = reviewDao.getTrainerReviewCount(reviewBean.getTid()); //트레이너의 리뷰 수
+		if(reviewCount == 0) { //만약에 초기 값을 넣는다. 리뷰 없는 상태로 설정하는것.
+			reviewDao.insertBaseValue(reviewBean.getTid());
+		}
 		return getPage;
 	}
 }
--- FitQuest/src/main/java/review/controller/ReviewUpdateController.java
+++ FitQuest/src/main/java/review/controller/ReviewUpdateController.java
@@ -1,27 +1,65 @@
 package review.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import gym.model.GymDao;
+import member.model.MemberBean;
+import member.model.MemberDao;
 import review.model.ReviewBean;
 import review.model.ReviewDao;
+import trainer.model.TrainerBean;
+import trainer.model.TrainerDao;
 
 @Controller
 public class ReviewUpdateController {
 	private final String command = "updateMyReview.mb";
 	private final String getPage = "myReviewUpdateForm";
+	private final String gotoPage = "redirect:/viewMyReviewList.mb";
 	
 	@Autowired
 	ReviewDao reviewDao;
-	
+	@Autowired
+	TrainerDao trainerDao;
+	@Autowired
+	MemberDao memberDao;
+	@Autowired
+	GymDao gymDao;
 	@RequestMapping(value = command, method = RequestMethod.GET)
-	public String doAction(@RequestParam("renum") int renum, Model model) {
+	public String doAction(@RequestParam("renum") int renum, 
+						   Model model,
+						   @RequestParam("mid") String mid) {
 		ReviewBean reviewBean = reviewDao.getReviewByRenum(renum);
+		MemberBean memberBean = memberDao.selectMemberById(mid);
+		System.out.println(reviewBean.getTid());
+		TrainerBean trainerBean = trainerDao.getTrainerMember(reviewBean.getTid());
 		model.addAttribute("reviewBean", reviewBean);
+		model.addAttribute("memberBean", memberBean);
+		model.addAttribute("tmemberBean", memberDao.selectMemberById(trainerBean.getId()));
+		model.addAttribute("trainerBean", trainerBean);
+		model.addAttribute("gymBean", gymDao.selectGym(trainerBean.getGnum()));
 		return getPage;
+	}
+	@RequestMapping(value = command, method = RequestMethod.POST)
+	public String doAction(@ModelAttribute("reviewBean") @Valid ReviewBean reviewBean,
+						   BindingResult result,
+						   Model model) {
+		if(result.hasErrors()) {
+			return getPage;
+		} else {
+			int cnt = reviewDao.updateReview(reviewBean);
+			if(cnt > 0) {
+				System.out.println("리뷰 수정 성공");
+			}
+		}
+		return gotoPage;
 	}
 }
