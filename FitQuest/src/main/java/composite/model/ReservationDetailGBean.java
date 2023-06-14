6936ad24f18a2d98a5d3e60fa4bffffe229432c7
dbsktla <dbsktla@naver.com>
2023-06-14 16:30:51 +0900

    [유나] composite 수정 06-14 16:30

---
+++ FitQuest/src/main/java/composite/model/ReservationDetailGBean.java
@@ -0,0 +1,75 @@
+package composite.model;
+
+public class ReservationDetailGBean {
+	private String name;
+	private String rdate;
+	private String rtime;
+	private String mphone;
+	private String gname;
+	private String gaddr1;
+	private String gaddr2;
+	private String gphone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRdate() {
+		return rdate;
+	}
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+	public String getRtime() {
+		return rtime;
+	}
+	public void setRtime(String rtime) {
+		this.rtime = rtime;
+	}
+	public String getMphone() {
+		return mphone;
+	}
+	public void setMphone(String mphone) {
+		this.mphone = mphone;
+	}
+	public String getGname() {
+		return gname;
+	}
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	public String getGaddr1() {
+		return gaddr1;
+	}
+	public void setGaddr1(String gaddr1) {
+		this.gaddr1 = gaddr1;
+	}
+	public String getGaddr2() {
+		return gaddr2;
+	}
+	public void setGaddr2(String gaddr2) {
+		this.gaddr2 = gaddr2;
+	}
+	public String getGphone() {
+		return gphone;
+	}
+	public void setGphone(String gphone) {
+		this.gphone = gphone;
+	}
+	public ReservationDetailGBean(String name, String rdate, String rtime, String mphone, String gname, String gaddr1,
+			String gaddr2, String gphone) {
+		super();
+		this.name = name;
+		this.rdate = rdate;
+		this.rtime = rtime;
+		this.mphone = mphone;
+		this.gname = gname;
+		this.gaddr1 = gaddr1;
+		this.gaddr2 = gaddr2;
+		this.gphone = gphone;
+	}
+	public ReservationDetailGBean() {
+		
+	}
+}
