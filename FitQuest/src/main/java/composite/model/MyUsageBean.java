340adbf08a11594fddb80fd6c2dc2c16ec6ab22b
jwm21542 <jwm21542@gmail.com>
2023-06-14 17:18:35 +0900

    [종원] 구매관리 관련 파일 추가 06-14 05:18pm

---
+++ FitQuest/src/main/java/composite/model/MyUsageBean.java
@@ -0,0 +1,99 @@
+package composite.model;
+
+public class MyUsageBean {
+	private int unum; //사용권 번호
+	private int pnum; //상품 번호
+	private int months; //사용권 사용기간
+	private int usage; //사용권 남은 횟수
+	private int ptime; //수업 길이
+	private String gname; //헬스장 명 
+	private String activity; //운동 종류
+	private String name; //트레이너 이름.
+	private String ptype; //개인/그룹 수업
+	private String timage; //트레이너 
+	private String tid; //트레이너 id
+	public MyUsageBean() {
+		super();
+	}
+	public MyUsageBean(int unum, int pnum, int months, int usage, int ptime, String gname, String activity, String name,
+			String ptype, String timage, String tid) {
+		super();
+		this.unum = unum;
+		this.pnum = pnum;
+		this.months = months;
+		this.usage = usage;
+		this.ptime = ptime;
+		this.gname = gname;
+		this.activity = activity;
+		this.name = name;
+		this.ptype = ptype;
+		this.timage = timage;
+		this.tid = tid;
+	}
+	public String getTid() {
+		return tid;
+	}
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+	public String getTimage() {
+		return timage;
+	}
+	public void setTimage(String timage) {
+		this.timage = timage;
+	}
+	public String getPtype() {
+		return ptype;
+	}
+	public void setPtype(String ptype) {
+		this.ptype = ptype;
+	}
+	public int getUnum() {
+		return unum;
+	}
+	public void setUnum(int unum) {
+		this.unum = unum;
+	}
+	public int getPnum() {
+		return pnum;
+	}
+	public void setPnum(int pnum) {
+		this.pnum = pnum;
+	}
+	public int getMonths() {
+		return months;
+	}
+	public void setMonths(int months) {
+		this.months = months;
+	}
+	public int getUsage() {
+		return usage;
+	}
+	public void setUsage(int usage) {
+		this.usage = usage;
+	}
+	public int getPtime() {
+		return ptime;
+	}
+	public void setPtime(int ptime) {
+		this.ptime = ptime;
+	}
+	public String getGname() {
+		return gname;
+	}
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	public String getActivity() {
+		return activity;
+	}
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
