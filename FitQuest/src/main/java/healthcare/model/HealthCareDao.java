a112367a817bf59e77132c8312cd1b1b52a23835
zenzen <aaaaru66@naver.com>
2023-06-19 05:24:24 +0900

    [아연] 트레이너-회원목록 조회 06-19 05:24

---
+++ FitQuest/src/main/java/healthcare/model/HealthCareDao.java
@@ -0,0 +1,23 @@
+package healthcare.model;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCareDao {
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	private String namespace = "healthcare.model.HealthCare";
+	
+	public List<HealthCareBean> getHealthCareList(Map<String, Object> map) {
+		List<HealthCareBean> hclist = sqlSessionTemplate.selectList(namespace + ".GetHealthCareList", map);
+		return hclist;
+	}
+	
+	
+}
