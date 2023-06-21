1dc62bd9ec22bc6975ea9b0b64c9908fafd8d665
dbsktla <dbsktla@naver.com>
2023-06-21 10:01:26 +0900

    [유나] 06-21 10:01

--- FitQuest/src/main/java/complete/model/CompleteDao.java
+++ FitQuest/src/main/java/complete/model/CompleteDao.java
@@ -4,10 +4,18 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import reservation.model.ReservationBean;
+
 @Component
 public class CompleteDao {
 	private final String namespace = "complete.model.Complete";
 	
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
+
+	public int insertComplete(ReservationBean rb) {
+		int cnt = -1;
+		cnt = sqlSessionTemplate.insert(namespace+".InsertComplete",rb);
+		return cnt;
+	}
 }
